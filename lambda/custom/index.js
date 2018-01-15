5509283be727ccd32f6fb7e75f05bb984b4cbdb3
Jan Klimo <jan.klimo@gmail.com>
2018-01-15 20:00:54 +0700

    project init

---
+++ lambda/custom/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+const request = require('request');
+const APP_ID = "amzn1.ask.skill.e32de1a7-6c49-4842-a156-fec4a1415bc8";
+
+const WELCOME_MESSAGE = 'Welcome to Blockfolio! Ask me about your crypto balances.';
+const WELCOME_REPROMPT = 'You can say what\'s my balance.';
+const HELP_MESSAGE = 'You can say what\'s my balance, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+
+exports.handler = function(event, context, callback) {
+  var alexa = Alexa.handler(event, context);
+  alexa.appId = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
+var handlers = {
+  'LaunchRequest': function () {
+    this.response.speak(WELCOME_MESSAGE).listen(WELCOME_REPROMPT);
+    this.emit(':responseReady');
+  },
+
+  'SessionEndedRequest' : function() {
+    console.log('Session ended with reason: ' + this.event.request.reason);
+  },
+
+  'GetCurrentBalanceIntent' : function () {
+    const speechOutput = 'Your current balance is $1000.'
+    this.response.speak(speechOutput);
+    getBalance((err, data) => console.log(data));
+    this.emit(':responseReady');
+  },
+
+  'AMAZON.StopIntent': function () {
+    this.response.speak(STOP_MESSAGE);
+    this.emit(':responseReady');
+  },
+
+  'AMAZON.HelpIntent': function () {
+    this.response.speak(HELP_MESSAGE).listen(HELP_REPROMPT);
+    this.emit(':responseReady');
+  },
+
+  'AMAZON.CancelIntent' : function() {
+    this.response.speak(STOP_MESSAGE);
+    this.emit(':responseReady');
+  },
+
+  'Unhandled' : function() {
+    this.response.speak("Sorry, I didn't get that. You can try: " +
+                        "'Alexa, what's my current balance'");
+  }
+};
+
+const getBalance = (callback) => {
+  const url = "https://api-v0.blockfolio.com/rest/get_all_positions/00c9c473b297e87ceab1b8627a3e54b0898804eb4aa72da310c0d444c62d5a44";
+  request.get(url, (error, response, body) => {
+    callback(error, JSON.parse(body));
+  });
+}
---
+++ lambda/custom/test.js
@@ -0,0 +1,144 @@
+'use strict'
+
+let expect = require('chai').expect;
+let lambdaToTest = require('./index');
+let balances = require('../fixtures/balances.json');
+const sinon = require('sinon');
+const request = require('request');
+
+class Context {
+  constructor() {
+    this.speechResponse = null;
+    this.speechError = null;
+  }
+
+  succeed(rsp) {
+    this.speechResponse = rsp;
+    this.done();
+  };
+
+  fail(rsp) {
+    this.speechError = rsp;
+    this.done();
+  };
+}
+
+let event = {
+  session: {
+    new: false,
+    sessionId: 'session1234',
+    attributes: {},
+    user: {
+      userId: 'usrid123'
+    },
+    application: {
+      applicationId: 'amzn1.ask.skill.e32de1a7-6c49-4842-a156-fec4a1415bc8'
+    }
+  },
+  version: '1.0',
+  request: {
+    intent: {
+      slots: {
+        SlotName: {
+          name: 'SlotName',
+          value: 'slot value'
+        }
+      },
+      name: 'intent name'
+    },
+    type: 'IntentRequest',
+    requestId: 'request5678'
+  }
+};
+
+const validRsp = (ctx, options) => {
+  expect(ctx.speechError).to.be.null;
+  expect(ctx.speechResponse.version).to.be.equal('1.0');
+  expect(ctx.speechResponse.response).not.to.be.undefined;
+  expect(ctx.speechResponse.response.outputSpeech).not.to.be.undefined;
+  expect(ctx.speechResponse.response.outputSpeech.type).to.be.equal('SSML');
+  expect(ctx.speechResponse.response.outputSpeech.ssml).not.to.be.undefined;
+  expect(ctx.speechResponse.response.outputSpeech.ssml).to.match(/<speak>.*<\/speak>/);
+
+  if(options.endSession) {
+    expect(ctx.speechResponse.response.shouldEndSession).to.be.true;
+    expect(ctx.speechResponse.response.reprompt).to.be.undefined;
+  } else {
+    expect(ctx.speechResponse.response.shouldEndSession).to.be.false;
+    expect(ctx.speechResponse.response.reprompt.outputSpeech).to.be.not.undefined;
+    expect(ctx.speechResponse.response.reprompt.outputSpeech.type).to.be.equal('SSML');
+    expect(ctx.speechResponse.response.reprompt.outputSpeech.ssml).to.match(/<speak>.*<\/speak>/);
+  }
+}
+
+const validCard = (ctx, standardCard) => {
+  expect(ctx.speechResponse.response.card).not.to.be.undefined;
+  expect(ctx.speechResponse.response.card.title).not.to.be.undefined;
+
+  if (standardCard) {
+    expect(ctx.speechResponse.response.card.type).to.be.equal('Standard');
+    expect(ctx.speechResponse.response.card.text).not.to.be.undefined;
+    expect(ctx.speechResponse.response.card.image).not.to.be.undefined;
+    expect(ctx.speechResponse.response.card.image.largeImageUrl).to.match(/^https:\/\//);
+    expect(ctx.speechResponse.response.card.image.smallImageUrl).to.match(/^https:\/\//);
+  } else {
+    expect(ctx.speechResponse.response.card.type).to.be.equal('Simple');
+    expect(ctx.speechResponse.response.card.content).not.to.be.undefined;
+  }
+}
+
+describe('All intents', () => {
+  let ctx = new Context();
+
+  describe('Test LaunchIntent', () => {
+    before((done) => {
+      event.request.type = 'LaunchRequest';
+      event.request.intent = {};
+      event.session.attributes = {};
+      ctx.done = done;
+      lambdaToTest.handler(event, ctx);
+    });
+
+    it('valid response', () => {
+      validRsp(ctx,{ endSession: false });
+    });
+
+    it('valid outputSpeech', () => {
+      expect(ctx.speechResponse.response.outputSpeech.ssml).to.match(/Welcome/);
+    });
+
+    it('valid repromptSpeech', () => {
+      expect(ctx.speechResponse.response.reprompt.outputSpeech.ssml)
+        .to.match(/You can say what's my balance/);
+    });
+  });
+
+  describe('Test GetCurrentBalanceIntent', () => {
+    before((done) => {
+      event.request.intent = {};
+      event.session.attributes = {};
+      event.request.type = 'IntentRequest';
+      event.request.intent.name = 'GetCurrentBalanceIntent';
+      ctx.done = done;
+
+      // stub GET request
+      sinon
+        .stub(request, 'get')
+        .yields(null, null, JSON.stringify(balances));
+
+      lambdaToTest.handler(event, ctx);
+    });
+
+    after(() => {
+      request.get.restore();
+    });
+
+    it('valid response', () => {
+      validRsp(ctx,{ endSession: true });
+    });
+
+    it('valid outputSpeech', () => {
+      expect(ctx.speechResponse.response.outputSpeech.ssml).to.match(/current balance/);
+    });
+  });
+});
